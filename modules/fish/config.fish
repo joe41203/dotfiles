506fa1eb33143717afd98d2c17eb967ea64afb93
yuji.kinjo <kinjoyuji@gmail.com>
2022-04-14 15:22:18 +0900

    Update

--- modules/fish/config.fish
+++ modules/fish/config.fish
@@ -8,3 +8,9 @@ end
 
 set -x GOPATH "$HOME/go"
 set -x PATH "$HOME/go/bin" $PATH
+
+set -x GEM_HOME "$HOME/.gem"
+set -x PATH "$GEM_HOME/bin:$PATH"
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+set -x PATH "$PATH":"$HOME/.pub-cache/bin"
+set -x PATH "$PATH":"$HOME/fvm/default/bin"
