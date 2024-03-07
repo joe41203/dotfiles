c89c1e784647e9725d9766c9262442381aa5c673
kinjo <kinjoyuji@gmail.com>
2024-03-08 02:21:57 +0900

    Add git-open plugin to zsh plugins

--- modules/zsh/plugins.zsh
+++ modules/zsh/plugins.zsh
@@ -4,6 +4,7 @@ source $ZPLUG_HOME/init.zsh
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "paulirish/git-open"
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/dotenv", from:oh-my-zsh
 zplug "plugins/command-not-found", from:oh-my-zsh
