18bbe138cb1ca5048f0abafc9c5a47cf4550140d
kinjo <kinjoyuji@gmail.com>
2024-04-11 10:50:14 +0900

    Refactor git-diff-peco function to copy diff to clipboard

--- modules/zsh/functions/git-diff-peco.zsh
+++ modules/zsh/functions/git-diff-peco.zsh
@@ -1,10 +1,11 @@
 function git-diff-peco() {
-  local base_branch=$(git branch | peco --query "$LBUFFER" | tr -d '[:space:]' | tr -d '*')
-  local compare_branch=$(git branch | peco --query "$LBUFFER" | tr -d '[:space:]' | tr -d '*')
+	local base_branch=$(git branch | peco --query "$LBUFFER" | tr -d '[:space:]' | tr -d '*')
+	local compare_branch=$(git branch | peco --query "$LBUFFER" | tr -d '[:space:]' | tr -d '*')
 
-  if [ -n "$base_branch" ] && [ -n "$compare_branch" ]; then
-    git diff $base_branch $compare_branch --output=diff.txt | echo "Diff output saved to diff.txt"
-  else
-    echo "Branch selection cancelled."
-  fi
+	if [ -n "$base_branch" ] && [ -n "$compare_branch" ]; then
+		git diff $base_branch $compare_branch | pbcopy
+		echo "Git diff between $base_branch and $compare_branch has been copied to clipboard."
+	else
+		echo "Branch selection cancelled."
+	fi
 }
