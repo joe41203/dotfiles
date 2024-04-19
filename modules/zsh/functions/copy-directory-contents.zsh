db540e9d4b687ebf3f8b73437a32b664cd3aee7c
kinjo <kinjoyuji@gmail.com>
2024-04-19 21:06:19 +0900

    Rename function to 'copy-directory-contents'. Update echo message

--- modules/zsh/functions/copy-directory-contents.zsh
+++ modules/zsh/functions/copy-directory-contents.zsh
@@ -1,10 +1,10 @@
-function search-and-copy() {
+function copy-directory-contents() {
   local selected_dir
   selected_dir=$(find . -type d | fzf --preview 'ls -l {}' --bind 'ctrl-v:execute(less -f {})')
   if [[ -n $selected_dir ]]; then
     local file_contents
     file_contents=$(cd "$selected_dir" && tail -n+1 *)
     echo "$file_contents" | pbcopy
-    echo "${selected_dir} has been copied to clipboard."
+    echo "${selected_dir} contents have been copied to clipboard."
   fi
 }
