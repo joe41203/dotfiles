f32ba22de164cf2127a0ed2547b141e63d6a75fc
kinjo <kinjoyuji@gmail.com>
2024-05-14 00:17:28 +0900

    Copy directory contents with find and cat commands

--- modules/zsh/functions/copy-directory-contents.zsh
+++ modules/zsh/functions/copy-directory-contents.zsh
@@ -3,8 +3,8 @@ function copy-directory-contents() {
   selected_dir=$(find . -type d | fzf --preview 'ls -l {}' --bind 'ctrl-v:execute(less -f {})')
   if [[ -n $selected_dir ]]; then
     local file_contents
-    file_contents=$(cd "$selected_dir" && tail -n+1 *)
+    file_contents=$(cd "$selected_dir" && find . -type f -name "*.*" -exec cat {} \;)
     echo "$file_contents" | pbcopy
-    echo "${selected_dir} contents have been copied to clipboard."
+    echo "${selected_dir} 以下の全てのコードがクリップボードにコピーされました。"
   fi
 }
