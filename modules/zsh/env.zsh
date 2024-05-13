6fa2354f47cdb689c24b08278e60b0aa492eb8d2
kinjo <kinjoyuji@gmail.com>
2024-05-14 00:17:17 +0900

    Add Android SDK platform tools to PATH variable

--- modules/zsh/env.zsh
+++ modules/zsh/env.zsh
@@ -1,5 +1,6 @@
 export PATH="$HOME/.anyenv/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
+export PATH=$PATH:/Users/kinjo/Library/Android/sdk/platform-tools
 
 export TF_CLI_ARGS_plan="--parallelism=30"
 export TF_CLI_ARGS_apply="--parallelism=30"
