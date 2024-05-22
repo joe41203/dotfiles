14b4aa1e5697a535afa61a951deb849846d2c141
kinjo <kinjoyuji@gmail.com>
2024-05-22 15:19:40 +0900

    Update Android SDK path in zsh configuration

--- modules/zsh/env.zsh
+++ modules/zsh/env.zsh
@@ -1,6 +1,8 @@
 export PATH="$HOME/.anyenv/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
-export PATH=$PATH:/Users/kinjo/Library/Android/sdk/platform-tools
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$ANDROID_HOME/platform-tools:$PATH
 
 export TF_CLI_ARGS_plan="--parallelism=30"
 export TF_CLI_ARGS_apply="--parallelism=30"
