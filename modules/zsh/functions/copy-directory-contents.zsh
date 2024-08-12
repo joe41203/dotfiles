edf908fa2f1a750377ee8795a1e974daf3b084a0
kinjo <kinjoyuji@gmail.com>
2024-08-12 16:52:51 +0900

    Exclude hidden and unwanted dirs in copy function

--- modules/zsh/functions/copy-directory-contents.zsh
+++ modules/zsh/functions/copy-directory-contents.zsh
@@ -1,9 +1,22 @@
 function copy-directory-contents() {
   local selected_dir
-  selected_dir=$(find . -type d | fzf --preview 'ls -l {}' --bind 'ctrl-v:execute(less -f {})')
+  selected_dir=$(find . -type d \
+    -not -path '*/\.*' \
+    -not -path '*/__pycache__*' \
+    -not -path '*/node_modules*' \
+    -not -path '*/.git*' \
+    -not -path '*/.venv*' \
+    | fzf --preview 'ls -l {}' --bind 'ctrl-v:execute(less -f {})')
+
   if [[ -n $selected_dir ]]; then
     local file_contents
-    file_contents=$(cd "$selected_dir" && find . -type f -name "*.*" -exec cat {} \;)
+    file_contents=$(cd "$selected_dir" && find . -type f \
+      -not -path '*/\.*' \
+      -not -path '*/__pycache__*' \
+      -not -path '*/node_modules*' \
+      -not -path '*/.git*' \
+      -not -path '*/.venv*' \
+      -exec cat {} +)
     echo "$file_contents" | pbcopy
     echo "${selected_dir} 以下の全てのコードがクリップボードにコピーされました。"
   fi
